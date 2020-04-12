98eadd8a7b32adccb503514d356f11eae24ac848
Wonkadude <darius.wigglesworth@gmail.com>
2020-04-11 20:42:10 -0400

    Finished half of simulation

--- montyHall.cpp
+++ montyHall.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 using namespace std;
 
 int simulation(){
+    bool doors[3];
+    for(int i = 0; i < 3; i++){
+        doors[i] = false;
+    }
+    int carDoor = rand() % 3;
+    doors[cardDoor] = true;
+    
+    int chosenDoor = rand() % 3;
 
+    int hostDoor = rand() % 3;
+    while(hostDoor == chosenDoor){
+        hostDoor = rand() % 3;
+    }
 }
 
 int main (int argc, char** argv){
+
+    srand(time(NULL));
     int correctDoor = 0;
     
     if(argc != 2){
